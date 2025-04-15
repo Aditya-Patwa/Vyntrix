1fb2a9e485098159eda95a479d1344fbb45baed7
Aastha Malviya <141214756+aastha44ma@users.noreply.github.com>
2025-04-15 22:57:54 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const geistSans = Geist({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "VYNTRIX - Build. Connect. Deploy — On Solana, Seamlessly.",
+  description: "Create smart contracts visually, manage users and data in real-time, and launch full-stack dApps on Solana — all without leaving your browser.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.className} w-screen overflow-x-hidden`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          // enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
