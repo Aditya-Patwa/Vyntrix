ff6515b26bbfe4367b0c3189979a8bf1f2e20326
Aditya Patwa <adityapatwa.tech@gmail.com>
2025-04-24 23:00:56 +0530

    Some Cards Edit

--- app/dashboard/project/[id]/integrations/page.tsx
+++ app/dashboard/project/[id]/integrations/page.tsx
@@ -1,3 +1,24 @@
+import { PackageIcon } from "lucide-react"
+
+function PluginCard() {
+    return (
+        <div className="flex p-4 gap-2 rounded bg-neutral-900 border">
+            <div>
+                <PackageIcon />
+            </div>
+            <div className="grid">
+                <h1 className="text-base font-semibold tracking-tight">
+                    Storage
+                </h1>
+                <p className="text-muted-foreground text-sm">
+                    Decentralized file storage with IPFS
+                </p>
+            </div>
+        </div>
+    )
+}
+
+
 export default function Integrations() {
     return (
         <main className="grid">
@@ -6,14 +27,22 @@ export default function Integrations() {
                     Integrations
                 </h1>
             </div>
-            <div className="p-4 md:px-6 md:py-8 w-full border-b">
-                <div className="grid gap-1">
+            <div className="p-4 md:px-6 md:py-8 w-full border-b grid gap-8">
+                <div className="grid">
                     <h1 className="text-lg font-semibold tracking-tight">
                         All Integrations
                     </h1>
                     <p className="text-muted-foreground">
                         Extend your app capabilities by integrating other powerful technologies.
                     </p>
+                </div>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <PluginCard />
+                    <PluginCard />
+                    <PluginCard />
+                    <PluginCard />
+                    <PluginCard />
                 </div>
             </div>
         </main>
