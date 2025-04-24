2b2b9532aa9f446d6d4f1af9ee700538201cbd1e
Aditya Patwa <adityapatwa.tech@gmail.com>
2025-04-24 22:50:52 +0530

    Headline Added

--- app/dashboard/project/[id]/integrations/page.tsx
+++ app/dashboard/project/[id]/integrations/page.tsx
@@ -6,6 +6,16 @@ export default function Integrations() {
                     Integrations
                 </h1>
             </div>
+            <div className="p-4 md:px-6 md:py-8 w-full border-b">
+                <div className="grid gap-1">
+                    <h1 className="text-lg font-semibold tracking-tight">
+                        All Integrations
+                    </h1>
+                    <p className="text-muted-foreground">
+                        Extend your app capabilities by integrating other powerful technologies.
+                    </p>
+                </div>
+            </div>
         </main>
     )
 }
